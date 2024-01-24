23b12da3dbb6ee95a2e2f63ca0fb1e8be7ebad85
abuwael <mahmoudwaeldev@gmail.com>
2024-01-24 11:35:18 +0200

    check 👀🐱‍🚀

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +11,55 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Position? _currentLocation;
+    late bool servisePermition = false;
+    late LocationPermission permission;
+    String _currentaddress = "";
+    Future<Position> _getCurrentLocation() async {
+      servisePermition = await Geolocator.isLocationServiceEnabled();
+      if (!servisePermition) {
+        permission = await Geolocator.checkPermission();
+        if (permission == LocationPermission.denied) {
+          permission = await Geolocator.requestPermission();
+        } else if (permission == LocationPermission.denied) {
+          return Future.error("");
+        }
+      }
+      return await Geolocator.getCurrentPosition();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Find Location"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Location cordinates",
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 15),
+            const Text("Cordinates"),
+            const SizedBox(height: 15),
+            const Text(
+              "Location Address",
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            const Text("address"),
+            const SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: () async {
+                _currentLocation = await _getCurrentLocation();
+              },
+              child: const Text("get location"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
